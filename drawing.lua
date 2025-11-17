f260e4bb9766a1c29b9c11582ca24da31b5587f6
0xsomejs <somejs@proton.me>
2025-11-17 17:33:29 +0100

    Update drawing.lua

--- drawing.lua
+++ drawing.lua
@@ -886,6 +886,26 @@ getgenv().Drawing = {
     end
 }
 
+getgenv().isrenderobj = function(drawingObj)
+    local success, isrenderobj = pcall(function()
+		return drawingObj.Parent == drawingUI
+	end)
+	if not success then return false end
+	return isrenderobj
+end
+
+getgenv().getrenderproperty = function(drawingObj, property)
+	local success, drawingProperty  = pcall(function()
+		return drawingObj[property]
+	end)
+	if not success then return end
+
+	if drawingProperty ~= nil then
+		return drawingProperty
+	end
+end
+
+
 getgenv().setrenderproperty = function(drawingObj, property, value)
 	assert(getgenv().getrenderproperty(drawingObj, property), "'" .. tostring(property) .. "' is not a valid property of " .. tostring(drawingObj) .. ", " .. tostring(typeof(drawingObj)))
 	drawingObj[property]  = value
@@ -900,3 +920,5 @@ end
 Drawing = getgenv().Drawing
 cleardrawcache = getgenv().cleardrawcache
 setrenderproperty = getgenv().setrenderproperty
+getrenderproperty = getgenv().getrenderproperty
+isrenderobj = getgenv().isrenderobj
